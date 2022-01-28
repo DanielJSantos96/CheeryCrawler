f805c5742d8a5877c3d9df37076274c89564705f
Daniel Santos <danieljsantos.96@gmail.com>
2022-01-28 02:34:45 +0000

    Final

--- scrap/main.js
+++ scrap/main.js
@@ -14,10 +14,22 @@ Apify.main(async () => {
 			const kvs = await Apify.openKeyValueStore("product-pages");
 			await kvs.setValue(request.loadedUrl.replace('https://', '').replaceAll(/-|\//g,'_'), {html: body});
 		} else {
+			console.log("SITEIS:"+request.loadedUrl);
+			maxPgNrMatch = body.match(/(?<=var iMaxPgNr = parseInt\(")(\d+)/);
+			//maxPgNrMatch = body.match(/var iCurPgNr = parseInt\("(\d+)/);
+			if (!request.loadedUrl.includes('?pgNr=') && maxPgNrMatch) {
+				console.log(maxPgNrMatch[0]);
+				maxPgNr = Number(maxPgNrMatch[0]);
+				for (i = 1; i < maxPgNr; i++) {
+					await requestQueue.addRequest({ url: request.loadedUrl + '?pgNr='+i+'#jqList'});
+					console.log(request.loadedUrl + '?pgNr='+i+'#jqList');
+				}
+			}
+			
 			const regex = /out\/|\/modules\/|^(?!https:\/\/www.edeka24.de)/;
 			const baseLinks = $("[href]")
 					.filter((i, el) => {return ($(el).attr("href")!= "#" && !($(el).attr("href").match(regex)))})
-					.map((i,el) => {return ($(el).attr("href").includes("?force_sid=")) ? $(el).attr("href").substring(0, $(el).attr("href").indexOf("?force_sid=")) : $(el).attr("href")})
+					.map((i,el) => {return ($(el).attr("href").includes("force_sid=")) ? $(el).attr("href").substring(0, $(el).attr("href").indexOf("force_sid=")-1) : $(el).attr("href")})
 					.get();
 			
 			
@@ -30,7 +42,7 @@ Apify.main(async () => {
     };
 
     const crawler = new Apify.CheerioCrawler({
-		maxRequestsPerCrawl: 1000,
+		maxConcurrency: 3,
         requestQueue,
         handlePageFunction,
     });
